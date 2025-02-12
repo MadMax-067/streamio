e7c4fd25565e23f052785cec6294b084adc32e9e
MadMax-067 <gfxnxt@gmail.com>
2025-02-12 20:30:33 +0530

    mobile dashboard optimization

--- components/Hero.js
+++ components/Hero.js
@@ -11,11 +11,28 @@ const Hero = () => {
 
     return (
         <section className='hero inline-block md:p-4 max-h-[90.75vh] overflow-x-hidden overflow-y-scroll h-[90.75vh] md:w-[84.375vw]' >
-            {backendData.isLoggedIn && (<div className="flex h-fit flex-col md:flex-row flex-wrap justify-center gap-12 md:gap-x-5 md:gap-y-7 ">
-                {backendData.homeLoading ? <Loading /> : backendData.homeMessage ? <div>{backendData.homeMessage}</div> : (backendData.homeFeed?.map((video, index) => {
-                    return <VideoCard key={video._id} videoId={video._id} title={video.title} channelName={video.owner.username} thumbnail={video.thumbnail} views={video.views} duration={video.duration} avatar={video.owner.avatar} />
-                }))}
-            </div>)}
+            {backendData.isLoggedIn && (
+                <div className="flex h-fit flex-col md:flex-row flex-wrap items-center md:items-start justify-center gap-12 md:gap-x-5 md:gap-y-7 px-4 md:px-0">
+                    {backendData.homeLoading ? (
+                        <Loading />
+                    ) : backendData.homeMessage ? (
+                        <div>{backendData.homeMessage}</div>
+                    ) : (
+                        backendData.homeFeed?.map((video, index) => {
+                            return <VideoCard 
+                                key={video._id} 
+                                videoId={video._id} 
+                                title={video.title} 
+                                channelName={video.owner.username} 
+                                thumbnail={video.thumbnail} 
+                                views={video.views} 
+                                duration={video.duration} 
+                                avatar={video.owner.avatar} 
+                            />
+                        })
+                    )}
+                </div>
+            )}
         </section>
     )
 }
